98256755f0242c1ac6ca2b3f5b24aca4560def3f
Sayantan Basu <sayantanbasu@live.in>
2021-06-21 17:37:55 -0400

    added assignment table

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -15,3 +15,16 @@ CREATE TABLE students (
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
+
+CREATE TABLE assignments (
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(255),
+content VARCHAR(255),
+day VARCHAR(255),
+chapter VARCHAR(255),
+duration VARCHAR(255)
+);
+
+CREATE TABLE assignment_submission(
+  
+);
