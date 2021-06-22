4becc33ee3677bc68e943309ce32c754f5e19e33
Sayantan Basu <sayantanbasu@live.in>
2021-06-22 06:44:48 -0400

    added a query

--- 3_queries_group-by/total_assignments.sql
+++ 3_queries_group-by/total_assignments.sql
@@ -1,7 +1,13 @@
-SELECT cohort.name as cohort , count(assignment_submissions.*) AS total_submissions
+-- SELECT cohort.name AS cohort , count(assignment_submissions.*) AS total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- JOIN cohorts ON cohorts.id = cohort_id 
+-- GROUP BY cohorts.name
+-- ORDER BY count(assignment_submissions.*)
+-- ;
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
 FROM assignment_submissions
 JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id 
+JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
-ORDER BY count(assignment_submissions.*)
-;
+ORDER BY total_submissions DESC;
