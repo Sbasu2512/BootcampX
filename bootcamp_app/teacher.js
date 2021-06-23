eca8d292487486532d9d9d6428af7bb1fd26aef5
Sayantan Basu <sayantanbasu@live.in>
2021-06-23 17:08:44 -0400

    adding query

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -7,7 +7,10 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query().then(res => {
+pool.query(`SELECT count(assistance_requests.*) as total_assistances, teachers.name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+GROUP BY teachers.name; `).then(res => {
   console.log(res.rows);
 }).catch(error => {
   console.log("Error :", error);
