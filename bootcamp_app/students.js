0585a8fd223850877cf6ff4b70c9be4b29966316
Sayantan Basu <sayantanbasu@live.in>
2021-06-23 17:58:01 -0400

    parameterized

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,14 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const cohortsName = process.argv[2];
+const limit = process.argv[3]
+const values = [cohortsName, limit]
+//console.log(values);
+
+const query = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort 
+FROM students 
+JOIN cohorts ON cohorts.id = cohort_id 
+WHERE cohorts.name = $1 
+LIMIT $2;`
+
+pool.query(query, values)
 .then(res => {
+  //console.log(res.rows);
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
